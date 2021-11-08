a3475f5ee1779e3a43f013a68e49a4b6c8dfb448
miZyind <mizyind@gmail.com>
2021-11-09 01:52:30 +0700

    feat: wasm support

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,18 @@ mod lib;
 mod player;
 mod world;
 
-use bevy::{input::system::exit_on_esc_system, prelude::*};
+#[cfg(target_arch = "native")]
+use bevy::input::system::exit_on_esc_system;
+
+use bevy::prelude::*;
 use bevy_rapier3d::{physics::TimestepMode, prelude::*};
+
 use lib::rapier::RapierDynamicForceControllerPlugin;
 
 fn main() {
-    App::build()
+    let mut app = App::build();
+
+    app.insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .insert_resource(WindowDescriptor {
             title: "Singularity".to_string(),
             width: 800.0,
@@ -22,9 +28,15 @@ fn main() {
             timestep_mode: TimestepMode::InterpolatedTimestep,
             ..Default::default()
         })
-        .add_plugin(RapierDynamicForceControllerPlugin)
-        .add_system(exit_on_esc_system.system())
-        .add_startup_system(world::spawn.system())
+        .add_plugin(RapierDynamicForceControllerPlugin);
+
+    #[cfg(target_arch = "native")]
+    app.add_system(exit_on_esc_system.system());
+
+    #[cfg(target_arch = "wasm32")]
+    app.add_plugin(bevy_webgl2::WebGL2Plugin);
+
+    app.add_startup_system(world::spawn.system())
         .add_startup_system(player::spawn.system())
         .add_startup_system(camera::spawn.system())
         .add_system(camera::zoom.system())
