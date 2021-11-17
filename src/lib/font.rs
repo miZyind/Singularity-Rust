a2563872303a7247a8397c5286868639fe786363
miZyind <mizyind@gmail.com>
2021-11-17 22:55:53 +0700

    feat: add font crate

---
+++ src/lib/font.rs
@@ -0,0 +1,9 @@
+use bevy::{prelude::Res, window::Windows};
+
+// 3840 = 288pt = 200.0%;
+// 1920 = 144pt = 100.0%;
+// 1280 =  96pt = 66.66%;
+//  640 =  48pt = 33.33%;
+pub fn normalize(windows: Res<Windows>, font_size: i32) -> f32 {
+    windows.get_primary().unwrap().width() * font_size as f32 / 1920.0
+}
--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -3,6 +3,7 @@ pub mod controller;
 pub mod diagnostics;
 pub mod easing;
 pub mod events;
+pub mod font;
 pub mod inputs;
 pub mod look;
 pub mod rapier;
