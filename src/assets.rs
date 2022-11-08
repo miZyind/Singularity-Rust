4d6270758176663d69587c50a98dc919d1e4baf0
miZyind <mizyind@gmail.com>
2022-11-08 16:19:26 +0700

    refactor: model asset

--- src/assets.rs
+++ src/assets.rs
@@ -7,4 +7,6 @@ pub struct UIAssets {
     pub font: Handle<Font>,
     #[asset(path = "images/logo.png")]
     pub logo: Handle<Image>,
+    #[asset(path = "models/shiba.glb#Scene0")]
+    pub player: Handle<Scene>,
 }
